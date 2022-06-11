3e940cc7d2bb55d4575d08176017816766fd5188
khaledmv <khaledhossionmv@gmail.com>
2022-06-11 15:25:29 +0600

    shopify theme initilize

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./layout/**/*.liquid",
+    "./sections/**/*.liquid",
+    "./snippets/**/*.liquid",
+    "./templates/**/*.liquid",
+    "./templates/**/*.json",
+    "./templates/customers/**/*.liquid",
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
